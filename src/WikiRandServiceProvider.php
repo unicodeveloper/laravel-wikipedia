6440e2ea0bce4adba94464bbf78e41c38dc3bcb0
busayo <prosperotemuyiwa@gmail.com>
2015-10-20 10:05:28 +0100

    Add Facades and ServiceProvider

---
+++ src/WikiRandServiceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Busayo\Larapedia;
+
+use Illuminate\Support\ServiceProvider;
+
+class WikiRandServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Register the application services
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('wikirand', function(){
+            return new WikiRand;
+        });
+    }
+
+    /**
+     * Get the services provided by the provider
+     * @return array
+     */
+    public function provides()
+    {
+        return ['wikirand'];
+    }
+}
