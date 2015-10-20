49fd6964b1a968d603acc4160087f0ff9b5ea68e
busayo <prosperotemuyiwa@gmail.com>
2015-10-20 07:49:23 +0100

    Initial Commit

---
+++ tests/LarapediaTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Busayo\Larapedia\Test;
+
+class LarapediaTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test that true does in fact equal true
+     */
+    public function testTrueIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
